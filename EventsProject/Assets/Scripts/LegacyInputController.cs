8f90003cc56c361d687f79fee1e324fb3202b5b4
Taras Makarevich <tarasm@playtika.com>
2020-10-20 11:47:38 +0300

    Implement simple legacy input controller.

--- EventsProject/Assets/Scripts/LegacyInputController.cs
+++ EventsProject/Assets/Scripts/LegacyInputController.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LegacyInputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform cubeTransform;
+    [SerializeField]
+    private Transform aimTransform;
+    [SerializeField]
+    private GameObject bulletPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private bool jump;
+    private bool shot;
+
+    private void Update()
     {
+        var pos = cubeTransform.position;
+        pos.x = Input.GetAxis("Horizontal");
+        cubeTransform.position = pos;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            var jumpSequence = DOTween.Sequence();
+            jumpSequence.Append(cubeTransform.DOJump(pos, 1.0f, 1, 0.3f));
+            jump = true;
+            jumpSequence.OnComplete(() => { jump = false; });
+        }
+
+        if (!Input.GetKey(KeyCode.W))
+        {
+            aimTransform.gameObject.SetActive(false);
+            return;
+        }
+
+        aimTransform.gameObject.SetActive(true);
         
+        var direction = Input.GetAxis("Mouse X");
+        pos = aimTransform.position;
+        pos.x += direction * Time.deltaTime * 5.0f;
+        pos.x = Mathf.Clamp(pos.x, -1.0f, 1.0f);
+        aimTransform.position = pos;
+
+        if (!Input.GetMouseButtonDown(0) || shot)
+        {
+            return;
+        }
+
+        var bullet = Instantiate(bulletPrefab, aimTransform.position, Quaternion.identity);
+        shot = true;
+        var shotSequence = DOTween.Sequence();
+        shotSequence.Append(bullet.transform.DOMoveZ(10.0f, 1.0f));
+        shotSequence.OnComplete(() => 
+            { 
+                Destroy(bullet);
+                shot = false; 
+            });
     }
 }
