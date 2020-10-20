0d88c4f5266576b4792ed6676027a673abcdd1f3
dariak <dariak@playtika.com>
2020-10-20 14:15:07 +0300

    Initial commit

---
+++ EventsProject/Assets/Scripts/InputController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    private void Start()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+        playerInput.onActionTriggered += OnInputAction;
+    }
+
+    private void OnInputAction(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+        {
+            return;
+        }
+
+        switch (context.action.name)
+        {
+            case "FireAction":
+                Debug.Log("Fire Action");
+                break;
+            case "SkillAction":
+                Debug.Log("Skill Action");
+                break;
+            case "Horizontal":
+                Debug.Log($"Horizontal {context.ReadValue<float>()}");
+                break;
+            case "MousePosition":
+                Debug.Log($"Mouse position {context.ReadValue<Vector2>()}");
+                break;
+        }
+    }
+}
---
+++ EventsProject/Assets/Scripts/PlanetClicker/PlanetGameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class PlanetGameController : MonoBehaviour
+{
+    [SerializeField] private Sprite[] habitablePlanets;
+    [SerializeField] private Sprite[] inhabitablePlanets;
+    [SerializeField] private Sprite[] gasGiants;
+
+    [SerializeField] private float gameCircleTime = 5f;
+    [SerializeField] private int planetsCount = 24;
+    [SerializeField] private Planet basePlanet;
+
+    private Planet[] planets;
+    private IEnumerator Start()
+    {
+        planets = new Planet[planetsCount];
+        planets[0] = basePlanet;
+        for (int i = 1; i < planetsCount; i++)
+        {
+            planets[i] = Instantiate(basePlanet, basePlanet.transform.parent, false);
+        }
+        while (true)
+        {
+            var habitablePlanetNumber = Random.Range(0, planetsCount);
+            for (int i = 0; i < planetsCount; i++)
+            {
+                if (i == habitablePlanetNumber)
+                {
+                    planets[i].Setup(habitablePlanets[Random.Range(0, habitablePlanets.Length)], OnHabitablePlanetClicked);
+                    continue;
+                }
+
+                if (Random.Range(0, 3) == 0)
+                {
+                    planets[i].Setup(inhabitablePlanets[Random.Range(0, inhabitablePlanets.Length)], OnUninhabitablePlanetClicked);
+                    continue;
+                }
+                
+                planets[i].Setup(gasGiants[Random.Range(0, gasGiants.Length)], OnGasGiantClicked);
+            }
+            yield return new WaitForSeconds(gameCircleTime);
+        }
+    }
+
+    private void OnHabitablePlanetClicked()
+    {
+        Debug.Log("habitable clicked");
+    }
+    
+    private void OnUninhabitablePlanetClicked()
+    {
+        Debug.Log("inhabitable clicked");
+    }
+    private void OnGasGiantClicked()
+    {
+        Debug.Log("gas giant clicked");
+    }
+}
