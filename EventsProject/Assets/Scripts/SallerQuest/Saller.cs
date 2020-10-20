cef6c642ddceafb0cca3c2356f5a287188f2b2b6
Maxim Kruk <maximkr@playtika.com>
2020-10-20 16:15:37 +0300

    NotFull

---
+++ EventsProject/Assets/Scripts/SallerQuest/Saller.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Saller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private Image _item;
+    [SerializeField] private Camera camera;
+    [SerializeField] private SallerManager _manager;
+
+    private Sequence _sequence;
+    private Vector3 _defaultPos;
+    private string _name;
+
+    public Sprite Img
+    {
+        get => _item.sprite;
+    }
+    public void Setup(Sprite sprite, string name)
+    {
+        _item.sprite = sprite;
+        _name = name;
+    }
+
+    public void Drag()
+    {
+        var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+        _defaultPos = pos;
+        pos.z = 0f;
+        transform.position = pos;
+    }
+
+    public void EndDrag()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void Drop()
+    {
+        var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0f;
+        switch (_name)
+        {
+            case "Player":
+            {
+                if (_manager.Buy(this, _name))
+                {
+                    Destroy(gameObject);
+                }
+                transform.position = _defaultPos;
+                break;
+            }
+            case "Saller":
+            {
+                if (_manager.Buy(this, _name))
+                {
+                    Destroy(gameObject);
+                }
+
+                transform.position = _defaultPos;
+                break;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _sequence?.Kill();
+
+        _sequence.Append(transform.DOScale(Vector3.one * 2f, 1f))
+            .Append(transform.DOScale(Vector3.one, 1f));
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _sequence?.Kill();
+        transform.DOScale(Vector3.one, 0.3f);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one * 1.3f, 0.3f);
+    }
+}
---
+++ EventsProject/Assets/Scripts/SallerQuest/SallerManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SallerManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _playerItems;
+    [SerializeField] private Sprite[] _sallerItems;
+    [SerializeField] private Saller _basePItem;
+    [SerializeField] private Saller _baseSItem;
+    [SerializeField] private int _gold = 1000;
+    [SerializeField] private int _coast = 10;
+    [SerializeField] private Text _player;
+    [SerializeField] private Text _saller;
+
+    private int _playerGold = 0;
+    private int _sallerGold = 0;
+    private Saller[] _sItems;
+    private Saller[] _pItems;
+    void Start()
+    {
+        _pItems = new Saller[_playerItems.Length];
+        _sItems = new Saller[_sallerItems.Length];
+
+        for (int i = 0; i < _pItems.Length; i++)
+        {
+            _pItems[i] = Instantiate(_basePItem, _basePItem.transform.parent, true);
+            _pItems[i].Setup(_playerItems[Random.Range(0, _playerItems.Length)], "Player");
+        }
+        for (int i = 0; i < _sItems.Length; i++)
+        {
+            _sItems[i] = Instantiate(_baseSItem, _baseSItem.transform.parent, true);
+            _sItems[i].Setup(_sallerItems[Random.Range(0, _sallerItems.Length)], "Saller");
+        }
+
+        _playerGold = _sallerGold = _gold;
+        _player.text = $"Player: {_playerGold}";
+        _saller.text = $"Saller: {_sallerGold}";
+    }
+
+    public bool Buy(Saller item, string name)
+    {
+        switch (name)
+        {
+            case "Player":
+            {
+                if (_playerGold > _coast)
+                {
+                    _playerGold -= _coast;
+                    _sallerGold += _coast;
+                    var i = Instantiate(item, _baseSItem.transform.parent, true);
+                    i.Setup(item.Img, "Saller");
+                    _player.text = $"Player: {_playerGold}";
+                    _saller.text = $"Saller: {_sallerGold}";
+                    return true;
+                }
+                break;
+            }
+            case "Saller":
+            {
+                if (_sallerGold > _coast)
+                {
+                    _sallerGold -= _coast;
+                    _playerGold += _coast;
+                    var i = Instantiate(item, _basePItem.transform.parent, true);
+                    i.Setup(item.Img, "Player");
+                    _player.text = $"Player: {_playerGold}";
+                    _saller.text = $"Saller: {_sallerGold}";
+                    return true;
+                }
+
+                break;
+            }
+        }
+        return false;
+    }
+}
