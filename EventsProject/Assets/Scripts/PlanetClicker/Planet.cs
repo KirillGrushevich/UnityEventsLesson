2f94c23563aaaf3eddffe5d6e308d4244f61dae1
Taras Makarevich <tarasm@playtika.com>
2020-10-20 11:27:09 +0300

    Fix controller.

--- EventsProject/Assets/Scripts/PlanetClicker/Planet.cs
+++ EventsProject/Assets/Scripts/PlanetClicker/Planet.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using DG.Tweening;
 
-public class Planet : MonoBehaviour
+public class Planet : 
+    MonoBehaviour, 
+    IPointerClickHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image image;
+
+    private event Action OnPlanetClick;
+
+    public void Setup(Sprite sprite, Action onClickCallback)
     {
-        
+        image.color = Color.white;
+        image.sprite = sprite;
+        OnPlanetClick = onClickCallback;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        OnPlanetClick?.Invoke();
+        image.DOColor(Color.clear, 0.3f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
     }
 }
--- EventsProject/Assets/Scripts/PlanetClicker/PlanetGameController.cs
+++ EventsProject/Assets/Scripts/PlanetClicker/PlanetGameController.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlanetGameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Sprite[] habitablePlanets;
+    [SerializeField]
+    private Sprite[] uninhabitablePlanets;
+    [SerializeField]
+    private Sprite[] gasGiants; 
+    [SerializeField]
+    private float gameCircleTime = 5.0f;
+    [SerializeField]
+    private int planetsCount = 24;
+    [SerializeField]
+    private Planet basePlanet;
+
+    private Planet[] planets;
+
+    private IEnumerator Start()
     {
-        
+        planets = new Planet[planetsCount];
+        planets[0] = basePlanet;
+
+        for (int i = 1; i < planetsCount; i++)
+        {
+            planets[i] = Instantiate(basePlanet, basePlanet.transform.parent, false);
+        }
+
+        while (true)
+        {
+            var habitablePlanetNumber = Random.Range(0, planetsCount);
+
+            for (int i = 0; i < planetsCount; i++)
+            {
+                if (i == habitablePlanetNumber)
+                {
+                    planets[i].Setup(habitablePlanets[Random.Range(0, habitablePlanets.Length)], 
+                                     OnHabitablePlanetClick);
+                    continue;
+                }
+
+                if (Random.Range(0, 3) == 0)
+                {
+                    planets[i].Setup(uninhabitablePlanets[Random.Range(0, uninhabitablePlanets.Length)],
+                                     OnInhabitablePlanetClick);
+                    continue;
+                }
+
+                planets[i].Setup(gasGiants[Random.Range(0, gasGiants.Length)],
+                                 OnGasGiantClick);
+            }
+            
+            yield return new WaitForSeconds(gameCircleTime);
+        }    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnHabitablePlanetClick()
     {
-        
+        Debug.Log("OnHabitablePlanetClick");
+    }
+
+    private void OnInhabitablePlanetClick()
+    {
+        Debug.Log("OnInhabitablePlanetClick");
+    }
+
+    private void OnGasGiantClick()
+    {
+        Debug.Log("OnGasGiantClick");
     }
 }
