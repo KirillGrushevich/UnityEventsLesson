1c4ed5c6fe2e1b0ae987404a839c78a07f92abba
Taras Makarevich <tarasm@playtika.com>
2020-10-20 09:42:26 +0300

    Add DragImage script.

---
+++ EventsProject/Assets/Scripts/DragImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragImage : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private Image image;
+    
+    private void PotentialDrag()
+    {
+        image.color = Color.red;
+    }
+
+    private void BeginDrag()
+    {
+        transform.localScale = Vector3.one * 1.3f;
+    }
+
+    private void Drag()
+    {
+        var pos = camera.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0.0f;
+        transform.position = pos;
+    }
+}
