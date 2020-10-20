c2c031f6888a78762d34a103458a427f2b59e376
Taras Makarevich <tarasm@playtika.com>
2020-10-20 09:58:29 +0300

    Implement button events.

---
+++ EventsProject/Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonEvents : MonoBehaviour
+{
+    [SerializeField]
+    private Transform rotationImage;
+
+    private Sequence sequence;
+
+    public void Select()
+    {
+        transform.localScale = Vector3.one * 1.2f;
+    }
+
+    public void Deselect()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void UpdateSelected()
+    {
+        rotationImage.Rotate(Time.deltaTime * 90.0f * Vector3.forward);
+    }
+
+    public void Move()
+    {
+        sequence?.Kill();
+        sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(Vector3.one * 0.8f, 0.3f));
+        sequence.Append(transform.DOScale(Vector3.one, 0.3f));
+    }
+
+    public void Submit()
+    {
+        Debug.Log("Submit");
+    }
+
+    public void Cancel()
+    {
+        Debug.Log("Cancel");
+    }
+}
